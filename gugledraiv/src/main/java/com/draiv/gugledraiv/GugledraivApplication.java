b03243760187e69efecac71d1dd1dabc19017232
camila <camilafkapp@gmail.com>
2024-10-25 19:13:04 -0300

    first commit

---
+++ gugledraiv/src/main/java/com/draiv/gugledraiv/GugledraivApplication.java
@@ -0,0 +1,13 @@
+package com.draiv.gugledraiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugledraivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GugledraivApplication.class, args);
+	}
+
+}
---
+++ gugledraiv/src/test/java/com/draiv/gugledraiv/GugledraivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draiv.gugledraiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugledraivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
